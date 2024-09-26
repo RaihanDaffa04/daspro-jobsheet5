12873d650d6b78df5c1e30f40cc8165aa3306b94
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2024-09-26 10:20:38 +0800

    commit ternary operator

--- PemilihanBilangan25.java
+++ PemilihanBilangan25.java
@@ -3,13 +3,11 @@ import java.util.Scanner;
 public class PemilihanBilangan25 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String hasil;
         System.out.print("Masukkan sebuah angka: ");
         int angka = input.nextInt();
-        if (angka % 2 == 0) {
-            System.out.println("Angka " + angka + " termasuk bilangan genap");
-        } else {
-            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
-        }
+        hasil = angka % 2 == 0 ? "bilangan genap" : "bilangan ganjil";
+        System.out.println("Angka " + angka + " merupakan " + hasil);
         input.close();
     }
     
