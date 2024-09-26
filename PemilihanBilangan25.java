ddf8f524e8895651a82b2274aff6a5fcaa18a453
RaihanDaffa04 <raihandaffaizzuddin77@gmail.com>
2024-09-26 09:40:33 +0800

    commit percobaan 1

---
+++ PemilihanBilangan25.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class PemilihanBilangan25 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan sebuah angka: ");
+        int angka = input.nextInt();
+        if (angka % 2 == 0) {
+            System.out.println("Angka " + angka + " termasuk bilangan genap");
+        } else {
+            System.out.println("Angka " + angka + " termasuk bilangan ganjil");
+        }
+        input.close();
+    }
+    
+}
